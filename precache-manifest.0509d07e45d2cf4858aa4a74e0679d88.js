d95115e3ab65401568ecd80e198c211385a561aa
Ken Chang <ken_chang@mikotek.com.tw>
2021-01-24 20:30:38 +0800

    deploy

---
+++ precache-manifest.0509d07e45d2cf4858aa4a74e0679d88.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b652cf77fa9801a3f7",
+    "url": "/craw-fc-two/css/app.ef63d4f3.css"
+  },
+  {
+    "revision": "a6da385aad2827173794",
+    "url": "/craw-fc-two/css/chunk-20b8e206.edd686b4.css"
+  },
+  {
+    "revision": "6f6deb5bf1de0ecfc773",
+    "url": "/craw-fc-two/css/chunk-4671f6ee.2b385490.css"
+  },
+  {
+    "revision": "6f9fd5e22b3dd2466f5b",
+    "url": "/craw-fc-two/css/chunk-79861296.c880b8ce.css"
+  },
+  {
+    "revision": "da6c1f0ee143da57f6ef",
+    "url": "/craw-fc-two/css/chunk-vendors.170b08c0.css"
+  },
+  {
+    "revision": "0cdc0f019cae3e5e9b9c",
+    "url": "/craw-fc-two/css/list-detail.687081cd.css"
+  },
+  {
+    "revision": "d692cec48d1814e0eeff",
+    "url": "/craw-fc-two/css/login.509272a8.css"
+  },
+  {
+    "revision": "bac1d1d0a9439381253b",
+    "url": "/craw-fc-two/css/login~seller.5dcb0138.css"
+  },
+  {
+    "revision": "6207d6d85f4c132685c7",
+    "url": "/craw-fc-two/css/seller.5c875e7f.css"
+  },
+  {
+    "revision": "360f842b3005ef65bc2a8f7a4d31eb91",
+    "url": "/craw-fc-two/index.html"
+  },
+  {
+    "revision": "87b652cf77fa9801a3f7",
+    "url": "/craw-fc-two/js/app.0c47dac5.js"
+  },
+  {
+    "revision": "a6da385aad2827173794",
+    "url": "/craw-fc-two/js/chunk-20b8e206.3727a055.js"
+  },
+  {
+    "revision": "6f6deb5bf1de0ecfc773",
+    "url": "/craw-fc-two/js/chunk-4671f6ee.cbe85f61.js"
+  },
+  {
+    "revision": "6f9fd5e22b3dd2466f5b",
+    "url": "/craw-fc-two/js/chunk-79861296.f5b9490e.js"
+  },
+  {
+    "revision": "da6c1f0ee143da57f6ef",
+    "url": "/craw-fc-two/js/chunk-vendors.904d3e50.js"
+  },
+  {
+    "revision": "469af7950bed503fe78c",
+    "url": "/craw-fc-two/js/dashboard.c312c03f.js"
+  },
+  {
+    "revision": "44972e3a282a0ee934b7",
+    "url": "/craw-fc-two/js/favorite.e7295061.js"
+  },
+  {
+    "revision": "0cdc0f019cae3e5e9b9c",
+    "url": "/craw-fc-two/js/list-detail.ccd30ed7.js"
+  },
+  {
+    "revision": "d692cec48d1814e0eeff",
+    "url": "/craw-fc-two/js/login.42a2607f.js"
+  },
+  {
+    "revision": "bac1d1d0a9439381253b",
+    "url": "/craw-fc-two/js/login~seller.3bf40916.js"
+  },
+  {
+    "revision": "6207d6d85f4c132685c7",
+    "url": "/craw-fc-two/js/seller.43c62be2.js"
+  },
+  {
+    "revision": "66175981212440ebe8647904f25ede2b",
+    "url": "/craw-fc-two/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/craw-fc-two/robots.txt"
+  }
+]);
